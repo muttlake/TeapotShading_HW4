d1828e6a980bd9a23c7e40c20679d2ff4b7f1875
muttlake <shepbop@gmail.com>
2017-10-24 19:56:04 -0500

    Trying to make both kinds of shaders in the same file. Sort of working but shaderType not passing between shaders.

--- Timothy_Shepard_HW4_VSc/main.cpp
+++ Timothy_Shepard_HW4_VSc/main.cpp
@@ -26,22 +26,15 @@ GLuint shaderProgramID;
 GLuint vao = 0;
 GLuint vbo;
 GLuint vno;
-GLuint positionID, colorID, normalID, lcID;
+GLuint positionID, colorID, normalID, shadingTypeID;
 GLuint indexBufferID;
-GLuint positionBuffer, colorBuffer, normalBuffer, lcbuffer;
+GLuint positionBuffer, colorBuffer, normalBuffer;
 
 struct Vertex {
 	GLfloat x, y, z;
 	GLfloat nx, ny, nz;
 	GLfloat r, g, b, a;
 };
-
-glm::vec3 light1direction = glm::vec3(0.0f, -1.0f, 0.0f);
-glm::vec3 light1color = glm::vec3(1.0f, 0.1f, 0.1f);
-glm::vec3 light2direction = glm::vec3(-1.0f, 0.0f, 0.0f);
-glm::vec3 light2color = glm::vec3(0.1f, 1.0f, 0.1f);
-glm::vec3 light3direction = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 light3color = glm::vec3(0.1f, 0.1f, 1.0f);
 
 ///////////////////////////////////////////////////////////////
 // Read and Compile Shaders from tutorial
@@ -280,141 +273,6 @@ void printTriangleIndices(GLuint* triangleIndices, int numVertices)
 		std::cout << triangleIndices[i] << "\n";
 	}
 }
-//// Test making lighting here in glm
-
-glm::vec4* buildLightedColorsVec4s(Vertex* allVertices, int numVertices, glm::mat4 M)
-{
-	glm::vec4* lightedColors;
-	lightedColors = new glm::vec4[numVertices];
-
-	/*
-	std::cout << "Making lighted colors in glm\n";
-	std::cout << "Using matrix transform:\n";
-	std::cout << M[0][0] << " " << M[1][0] << " " << M[2][0] << " " << M[3][0] << endl;
-	std::cout << M[0][1] << " " << M[1][1] << " " << M[2][1] << " " << M[3][1] << endl;
-	std::cout << M[0][2] << " " << M[1][2] << " " << M[2][2] << " " << M[3][2] << endl;
-	std::cout << M[0][3] << " " << M[1][3] << " " << M[2][3] << " " << M[3][3] << endl;
-	*/
-
-	glm::vec3 ld1 = light1direction;
-	glm::vec3 ld2 = light2direction;
-	glm::vec3 ld3 = light3direction;
-	glm::vec3 lc1 = light1color;
-	glm::vec3 lc2 = light2color;
-	glm::vec3 lc3 = light3color;
-	glm::vec3 ld1T = glm::vec3(M * glm::vec4(ld1, 1.0f));
-	glm::vec3 ld2T = glm::vec3(M * glm::vec4(ld2, 1.0f));
-	glm::vec3 ld3T = glm::vec3(M * glm::vec4(ld3, 1.0f));
-	glm::vec3 ld1Tn = glm::normalize(ld1T);
-	glm::vec3 ld2Tn = glm::normalize(ld2T);
-	glm::vec3 ld3Tn = glm::normalize(ld3T);
-
-	GLfloat Ka = 0.1f;
-	GLfloat Kd = 0.3f;
-	GLfloat Ks = 0.6f;
-	GLfloat s = 3.0f;
-
-
-	for (int i = 0; i < numVertices; i++)
-	{
-		Vertex vertex;
-		vertex = allVertices[i];
-		glm::vec3 v = glm::vec3(vertex.x, vertex.y, vertex.z);
-		glm::vec4 objectColor = glm::vec4(vertex.r, vertex.g, vertex.b, vertex.a);
-		glm::vec3 n = glm::vec3(vertex.nx, vertex.ny, vertex.nz);
-
-
-		glm::vec3 vT = glm::vec3(M * glm::vec4(v, 1.0f));
-		glm::vec3 nT = glm::vec3(glm::transpose(glm::inverse(M)) * glm::vec4(n, 1.0f));
-
-		glm::vec3 e = glm::vec3(0.0f, 0.0f, 0.0f) - vT;
-
-		glm::vec3 r1 = glm::reflect(-ld1T, nT);
-		glm::vec3 r2 = glm::reflect(-ld2T, nT);
-		glm::vec3 r3 = glm::reflect(-ld3T, nT);
-
-		glm::vec3 nTn = glm::normalize(nT);
-		glm::vec3 en = glm::normalize(e);
-		glm::vec3 r1n = glm::normalize(r1);
-		glm::vec3 r2n = glm::normalize(r2);
-		glm::vec3 r3n = glm::normalize(r3);
-
-		//specular
-		glm::vec3 spec1 = lc1 * glm::pow(glm::max(glm::dot(r1n, en), 0.0f), s);
-		glm::vec3 spec2 = lc2 * glm::pow(glm::max(glm::dot(r2n, en), 0.0f), s);
-		glm::vec3 spec3 = lc3 * glm::pow(glm::max(glm::dot(r3n, en), 0.0f), s);
-		glm::vec3 specular = Ks * (spec1 + spec2 + spec3);
-
-		//diffuse
-		glm::vec3 diff1 = lc1 * glm::max(glm::dot(nTn, ld1Tn), 0.0f);
-		glm::vec3 diff2 = lc2 * glm::max(glm::dot(nTn, ld2Tn), 0.0f);
-		glm::vec3 diff3 = lc3 * glm::max(glm::dot(nTn, ld3Tn), 0.0f);
-		glm::vec3 diffuse = Kd * (diff1 + diff2 + diff3);
-
-		//ambient
-		glm::vec3 amb1 = lc1;
-		glm::vec3 amb2 = lc2;
-		glm::vec3 amb3 = lc3;
-		glm::vec3 ambient = Ka * (amb1 + amb2 + amb3);
-
-		glm::vec3 color = specular + diffuse + ambient;
-		glm::vec4 outputColor = objectColor * glm::vec4(color, 1.0f);
-
-		/*
-		if (i % 1000 == 0) {
-			std::cout << "----------------------------------------------------------------------\n";
-			std::cout << endl << "v = " << v[0] << ", " << v[1] << ", " << v[2] << endl;
-			std::cout << "vT = " << vT[0] << ", " << vT[1] << ", " << vT[2] << endl;
-			std::cout << "n = " << n[0] << ", " << n[1] << ", " << n[2] << endl;
-			std::cout << "nT = " << nT[0] << ", " << nT[1] << ", " << nT[2] << endl;
-			std::cout << "nTn = " << nTn[0] << ", " << nTn[1] << ", " << nTn[2] << endl << endl;
-			std::cout << "ld1 = " << ld1[0] << ", " << ld1[1] << ", " << ld1[2] << endl;
-			std::cout << "ld1T = " << ld1T[0] << ", " << ld1T[1] << ", " << ld1T[2] << endl;
-			std::cout << "ld1Tn = " << ld1Tn[0] << ", " << ld1Tn[1] << ", " << ld1Tn[2] << endl;
-			std::cout << "ld2 = " << ld2[0] << ", " << ld2[1] << ", " << ld2[2] << endl;
-			std::cout << "ld2T = " << ld2T[0] << ", " << ld2T[1] << ", " << ld2T[2] << endl;
-			std::cout << "ld2Tn = " << ld2Tn[0] << ", " << ld2Tn[1] << ", " << ld2Tn[2] << endl;
-			std::cout << "ld3 = " << ld3[0] << ", " << ld3[1] << ", " << ld3[2] << endl;
-			std::cout << "ld3T = " << ld3T[0] << ", " << ld3T[1] << ", " << ld3T[2] << endl;
-			std::cout << "ld3Tn = " << ld3Tn[0] << ", " << ld3Tn[1] << ", " << ld3Tn[2] << endl << endl;
-			//std::cout << "eyePosition = " << eyePosition[0] << ", " << eyePosition[1] << ", " << eyePosition[2] << endl;
-			std::cout << "e = " << e[0] << ", " << e[1] << ", " << e[2] << endl;
-			std::cout << "en = " << en[0] << ", " << en[1] << ", " << en[2] << endl << endl;
-			std::cout << "r1 = " << r1[0] << ", " << r1[1] << ", " << r1[2] << endl;
-			std::cout << "r1n = " << r1n[0] << ", " << r1n[1] << ", " << r1n[2] << endl;
-			std::cout << "r2 = " << r2[0] << ", " << r2[1] << ", " << r2[2] << endl;
-			std::cout << "r2n = " << r2n[0] << ", " << r2n[1] << ", " << r2n[2] << endl;
-			std::cout << "r3 = " << r3[0] << ", " << r3[1] << ", " << r3[2] << endl;
-			std::cout << "r3n = " << r3n[0] << ", " << r3n[1] << ", " << r3n[2] << endl << endl;
-			std::cout << "lc1 = " << lc1[0] << ", " << lc1[1] << ", " << lc1[2] << endl;
-			std::cout << "lc2 = " << lc2[0] << ", " << lc2[1] << ", " << lc2[2] << endl;
-			std::cout << "lc3 = " << lc3[0] << ", " << lc3[1] << ", " << lc3[2] << endl << endl;
-			std::cout << "Ks = " << Ks << " , s = " << s << endl;
-			std::cout << "spec1 = " << spec1[0] << ", " << spec1[1] << ", " << spec1[2] << endl;
-			std::cout << "spec2 = " << spec2[0] << ", " << spec2[1] << ", " << spec2[2] << endl;
-			std::cout << "spec3 = " << spec3[0] << ", " << spec3[1] << ", " << spec3[2] << endl;
-			std::cout << "specular = " << specular[0] << ", " << specular[1] << ", " << specular[2] << endl << endl;
-			std::cout << "Kd = " << Kd << endl;
-			std::cout << "diff1 = " << diff1[0] << ", " << diff1[1] << ", " << diff1[2] << endl;
-			std::cout << "diff2 = " << diff2[0] << ", " << diff2[1] << ", " << diff2[2] << endl;
-			std::cout << "diff3 = " << diff3[0] << ", " << diff3[1] << ", " << diff3[2] << endl;
-			std::cout << "diffuse = " << diffuse[0] << ", " << diffuse[1] << ", " << diffuse[2] << endl << endl;
-			std::cout << "Ka = " << Ka << endl;
-			std::cout << "amb1 = " << amb1[0] << ", " << amb1[1] << ", " << amb1[2] << endl;
-			std::cout << "amb2 = " << amb2[0] << ", " << amb2[1] << ", " << amb2[2] << endl;
-			std::cout << "amb3 = " << amb3[0] << ", " << amb3[1] << ", " << amb3[2] << endl;
-			std::cout << "ambient = " << ambient[0] << ", " << ambient[1] << ", " << ambient[2] << endl << endl;
-			std::cout << "color = " << color[0] << ", " << color[1] << ", " << color[2] << endl;
-			std::cout << "objectColor = " << objectColor[0] << ", " << objectColor[1] << ", " << objectColor[2] << ", " << objectColor[3] << endl;
-			std::cout << "outputColor = " << outputColor[0] << ", " << outputColor[1] << ", " << outputColor[2] << ", " << outputColor[3] << endl;
-		}
-		*/
-
-		lightedColors[i] = outputColor;
-	}
-	return lightedColors;
-}
-
 
 
 ///////////////////////////////////////////////////////////////
@@ -439,8 +297,11 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 {
 	GLuint MVPID = glGetUniformLocation(shaderProgramID, "MVP");
 	GLuint MVID = glGetUniformLocation(shaderProgramID, "MV");
+	shadingTypeID = glGetUniformLocation(shaderProgramID, "s_vShadingType");
 
-
+	//set shadingType boolean
+	GLint shadingType = 0; // no lights
+	
 	glm::mat4 M = glm::mat4(1.0f);
 	glm::mat4 P = glm::mat4(1.0f);
 	glm::mat4 V = glm::mat4(1.0f);
@@ -455,6 +316,7 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 		V = glm::lookAt(glm::vec3(10.0f, 10.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		MV = V*M;
 		MVP = P*V*M;
+		shadingType = 1;
 		break;
 
 	case '2':
@@ -465,6 +327,7 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 		V = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		MV = V*M;
 		MVP = P*V*M;
+		shadingType = 1;
 		break;
 
 	case '3':
@@ -474,6 +337,7 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 		V = glm::lookAt(glm::vec3(10.0f, 10.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		MV = V*M;
 		MVP = P*V*M;
+		shadingType = 2;
 		break;
 
 	case '4':
@@ -484,6 +348,7 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 		V = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		MV = V*M;
 		MVP = P*V*M;
+		shadingType = 2;
 		break;
 
 	default:
@@ -494,14 +359,12 @@ void switchMVP(unsigned char key, int xmouse, int ymouse)
 
 	glUniformMatrix4fv(MVPID, 1, GL_FALSE, &MVP[0][0]);
 	glUniformMatrix4fv(MVID, 1, GL_FALSE, &MV[0][0]);
+	cout << "shadingType value: " << shadingType << endl;
+	glUniform1i(shadingTypeID, shadingType);
 	glutPostRedisplay(); //request display() call ASAP
 }
 
-//struct Light {
-//	glm::vec3 direction;
-//	glm::vec3 color;
-//};
-
+// main program
 int main(int argc, char** argv) {
 
 	// Get vertices from Tris.txt file
@@ -556,12 +419,6 @@ int main(int argc, char** argv) {
 	glm::mat4 MV = V*M;
 	glm::mat4 MVP = P*V*M;
 
-	//test lighting in glm
-	glm::vec4* lightedColors;
-	lightedColors = new glm::vec4[numVertices];
-	lightedColors = buildLightedColorsVec4s(allVertices, numVertices, MV);
-
-
 	// Change for APPLE version
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -580,10 +437,6 @@ int main(int argc, char** argv) {
 	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
 	glBufferData(GL_ARRAY_BUFFER, NUM_VERTICES * sizeof(glm::vec3), vnormals, GL_STATIC_DRAW);
 
-	glGenBuffers(1, &lcbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, lcbuffer);
-	glBufferData(GL_ARRAY_BUFFER, NUM_VERTICES * sizeof(glm::vec4), lightedColors, GL_STATIC_DRAW);
-
 	positionID = glGetAttribLocation(shaderProgramID, "s_vPosition");
 	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
 	glVertexAttribPointer(positionID, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
@@ -596,18 +449,12 @@ int main(int argc, char** argv) {
 	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
 	glVertexAttribPointer(normalID, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
 
-	lcID = glGetAttribLocation(shaderProgramID, "s_lvColor");
-	glBindBuffer(GL_ARRAY_BUFFER, lcbuffer);
-	glVertexAttribPointer(lcID, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), 0);
-
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, NUM_INDICES * sizeof(GLuint), vindices, GL_STATIC_DRAW); // Put indices in buffer as Gluint
 
 	//use shader program
 	glUseProgram(shaderProgramID);
-
-
 
 	GLuint MVPID = glGetUniformLocation(shaderProgramID, "MVP");
 	glUniformMatrix4fv(MVPID, 1, GL_FALSE, &MVP[0][0]);
@@ -637,12 +484,18 @@ int main(int argc, char** argv) {
 	GLuint light3c = glGetUniformLocation(shaderProgramID, "light3color");
 	glUniform3fv(light3c, 1, glm::value_ptr(light3color));
 
+	//set shadingType boolean
+	GLint shadingType = 1;
+	shadingTypeID = glGetUniformLocation(shaderProgramID, "s_vShadingType");
+	glUniform1i(shadingTypeID, shadingType);
+
+	cout << "main shadingType value: " << shadingType << endl;
+
 	glutKeyboardFunc(switchMVP);
 
 	glEnableVertexAttribArray(positionID);
 	glEnableVertexAttribArray(colorID);
 	glEnableVertexAttribArray(normalID);
-	glEnableVertexAttribArray(lcID);
 
 	glutMainLoop();
 
@@ -651,7 +504,6 @@ int main(int argc, char** argv) {
 	delete[] vpositions;
 	delete[] vnormals;
 	delete[] vcolors;
-	delete[] lightedColors;
 
 	return 0;
 }
--- Timothy_Shepard_HW4_VSc/vertexShader.vsh
+++ Timothy_Shepard_HW4_VSc/vertexShader.vsh
@@ -3,7 +3,6 @@
 in vec3 s_vPosition;
 in vec4 s_vColor;
 in vec3 s_vNormal;
-in vec4 s_lvColor;
 
 uniform mat4 MVP;   // MVP Matrix
 uniform mat4 MV;    // Model View Matrix
@@ -12,6 +11,12 @@ uniform vec3 light1direction;
 uniform vec3 light2direction;
 uniform vec3 light3direction;
 
+uniform vec3 light1color;
+uniform vec3 light2color;
+uniform vec3 light3color;
+
+uniform int s_vShadingType;
+
 out vec3 n;
 out vec3 v;
 
@@ -19,17 +24,25 @@ out vec3 ld1;
 out vec3 ld2;
 out vec3 ld3;
 
-out vec4 objectColor;
+out vec3 lc1;
+out vec3 lc2;
+out vec3 lc3;
 
-out vec4 lightedObjectColor;
+out vec4 objectColor;
+out vec4 gouraudColor;
+
+out int shadingType;
 
 
 void main() {
 
-	//pass on to fragment shader
+	shadingType = s_vShadingType;
+
+	//For both phong and gouraud shading
+
+	//but pass on to fragment shader for phong shading
 	n = (transpose(inverse(MV))* vec4(s_vNormal, 0.0f)).xyz;
 	n = normalize(n);
-	//n = s_vNormal;
 
 	ld1 = -1 * (MV * vec4(light1direction, 0.0f)).xyz;
 	ld1 = normalize(ld1);
@@ -43,8 +56,58 @@ void main() {
 	v = (MV * vec4(s_vPosition, 1.0f)).xyz;
 
 	objectColor = s_vColor;
+	gouraudColor = objectColor;
 
-	lightedObjectColor = s_lvColor;
+	lc1 = light1color;
+	lc2 = light2color;
+	lc3 = light3color;
+
+	if (shadingType == 1) //gouraud shading
+	{
+		//eye vector
+		vec3 e = vec3(0, 0, 0) - v;
+		e = normalize(e);
+
+		//light reflection vectors
+		vec3 r1 = reflect(-ld1, n);
+		r1 = normalize(r1);
+		vec3 r2 = reflect(-ld2, n);
+		r2 = normalize(r2);
+		vec3 r3 = reflect(-ld3, n);
+		r3 = normalize(r3);
+
+		//Specular Color
+		float Ks = 0.6f;
+		float s = 3.0f; //specular power
+
+		vec3 spec1 = lc1 * pow(max(dot(r1, e), 0.0), s);
+		vec3 spec2 = lc2 * pow(max(dot(r2, e), 0.0), s);
+		vec3 spec3 = lc3 * pow(max(dot(r3, e), 0.0), s);
+
+		vec3 specular = Ks * (spec1 + spec2 + spec3);
+
+		//Diffuse Color
+		float Kd = 0.3f;
+
+		vec3 diff1 = lc1 * max(dot(n, ld1), 0.0);
+		vec3 diff2 = lc2 * max(dot(n, ld2), 0.0);
+		vec3 diff3 = lc3 * max(dot(n, ld3), 0.0);
+
+		vec3 diffuse = Kd * (diff1 + diff2 + diff3);
+
+		//Ambient Color
+		float Ka = 0.1f;
+
+		vec3 amb1 = lc1;
+		vec3 amb2 = lc2;
+		vec3 amb3 = lc3;
+
+		vec3 ambient = Ka * (amb1 + amb2 + amb3);
+
+		//calculate output color
+		vec4 c = vec4((specular + diffuse + ambient), 1.0f);
+		gouraudColor = c * objectColor;
+	}
 
 	//set vertex position for display in openGL
 	gl_Position = MVP * vec4(s_vPosition, 1.0f);
